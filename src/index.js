518d1c8bf8d0150be82c8cac137ea42b96f3ab6b
Allen Benny <allenbenny038@gmail.com>
2023-09-18 20:19:22 +0530

    enabled service worker in production mode

--- src/index.js
+++ src/index.js
@@ -10,11 +10,6 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-
-if (process.env.NODE_ENV === "development") {
+if (process.env.NODE_ENV === "production") {
   serviceWorkerRegistration.register();
 }
